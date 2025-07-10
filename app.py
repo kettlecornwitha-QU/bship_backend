e3bc3679a3dbc559fb6bf2fac49ae2568a7534d4
Kettlecornwitha-QU <kettle.corn.with.a.q.u@gmail.com>
2025-07-09 21:05:23 -0500

    fixed port

--- app.py
+++ app.py
@@ -122,6 +122,6 @@ def undo() -> Response:
 	return jsonify({"message": "Undo successful"})
 
 if __name__ == "__main__":
-	#port = int(os.environ.get("PORT", 10000))
-	#app.run(host="0.0.0.0", port=port)
-	app.run(debug=True, port=5000)
+	port = int(os.environ.get("PORT", 10000))
+	app.run(host="0.0.0.0", port=port)
+	#app.run(debug=True, port=5000)
